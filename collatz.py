8f50d683a2a13dd2ad7c3fb5444e79a5a21ce1dc
shaggy0798 <david.freer@gmail.com>
2017-02-08 15:27:49 -0500

    Collatz that refuses non-integers.

--- collatz.py
+++ collatz.py
@@ -7,9 +7,17 @@ def collatz(number):
         print(3*number + 1)
         return (3*number+1)
 
+num = 1
+while num == 1:
+    try:
+        userInput = int(input("Enter a number:"))
+        while userInput != 1:
+            userInput = collatz(userInput)
+    except ValueError:
+        print('Please enter an integer.')
+        continue
+    break
 
-userInput = int(input("Enter a number:"))
-while userInput != 1:
-    userInput = collatz(userInput)
+
 
 
