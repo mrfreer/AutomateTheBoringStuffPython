fb730c102b89a2e7f45fd5006a54df9b3665dee8
David <david.freer@gmail.com>
2017-02-24 23:24:31 -0500

    Checking lists for data.

---
+++ myPets.py
@@ -0,0 +1,7 @@
+#Type a pet name and check if the pet name is in a list
+myPets = ['Zophie', 'Pooka', 'Fat-tail']
+name = input('Enter a pet name:')
+if name not in myPets:
+    print('I do not have a pet named ' + name)
+else:
+    print(name + ' is my pet.')
